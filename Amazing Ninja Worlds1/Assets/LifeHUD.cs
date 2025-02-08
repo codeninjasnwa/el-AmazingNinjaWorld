4d114725225b323ab97d571742bfabe6779d5b02
codeninjasnwa <codeninjas.nwaustin1@gmail.com>
2025-02-08 11:20:01 -0600

    Amazing Ninja World

---
+++ Amazing Ninja Worlds1/Assets/LifeHUD.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeHUD : MonoBehaviour
+{
+    private GameObject[] hearts;
+    private int lives = 3;
+    public GameObject background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hearts = GameObject.FindGameObjectsWithTag("heart");
+    }
+    public void HurtPlayer() {
+        
+        lives -= 1;
+        hearts[lives].SetActive(false);
+        if (lives == 0) {
+            background.GetComponent<GameManager>().GameOver();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
